9f8d993a81bf5f096ae27239506dd0eecbc45898
nataliehitchcock <56402315+nataliehitchcock@users.noreply.github.com>
2019-11-09 13:44:27 -0700

    updated js file to try to debug and fix errors

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
-const studentList = document.querySelector('ul');
+const studentList = document.querySelector('.student-list');
 const eachStudent = studentList.children;
 const studentsPerPage = 10;
 
@@ -17,11 +17,13 @@ const totalPages = () => {
 }
 // Add a function which displays 10 students per page
 const showPage = (list, page) => {
-  for (let i = 0; i < eachStudent.length; i++) {
-    if (i < studentsPerPage) {
-       eachStudent[i].style.display= 'block';
+const startIndex = (page * studentsPerPage) - studentsPerPage;
+const endIndex = page * studentsPerPage;
+  for (let i = 0; i < list.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+       list[i].style.display= 'block';
     } else {
-        eachStudent[i].style.display= 'none';
+        list[i].style.display= 'none';
     }
 }
 }
@@ -35,6 +37,7 @@ function addPaginationLinks (studentList) {
   divPage.appendChild(newDiv);
   const ulPagLinks = document.createElement('ul');
   newDiv.appendChild(ulPagLinks);
+  showPage (studentList, 1);
   for (let i = 1; i <= totalPages; i++) {
      const li = document.createElement('li');
      ulPagLinks.appendChild(li);
@@ -46,6 +49,7 @@ function addPaginationLinks (studentList) {
      firstA.className = 'active';
      const aList = document.querySelectorAll('a');
      //Adding an event listener to each link
+
      for (let j = 1; j <= aList.length; j++) {
         a.addEventListener('click', (event) => {
            showPage(studentList, j);
@@ -59,26 +63,4 @@ function addPaginationLinks (studentList) {
 }
 //Calling Pagination function
 addPaginationLinks(studentList);
-   // Select page class in the div, put into a variable
-     let page = document.body.querySelector('.page');
-   // Create a div, give it the “pagination” class, and append it to the .page div
-     const pageDiv = document.createElement('div');
-     pageDiv.className = 'pagination';
-     page.appendChild(pageDiv);
-   // Add a ul to “pagination” div
-     let ulPage = document.createElement('ul');
-     pageDiv.append(ulPage);
-     let buttonPage = document.querySelector('.pagination ul');
-     // For every page add li and a tags with the page number text
-    for (let i = 1; i <= totalPages(); i++) {
-      let li = document.createElement('li');
-      let a = document.createElement('a');
-        a.href = '#';
-        a.textContent = i;
-        buttonPage.appendChild(li);
-        li.appendChild(a);
-      }
-// Add functionality to the pagination buttons so that they show and hide the correct items
-totalPages();
-tenStudents();
-addPaginationLinks();
+
